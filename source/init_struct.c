ffb05e1cb3628b73bed23e67595e7ce96002353e
Maxime CARABINA <maxime.carabina@epitech.eu>
2021-05-10 16:27:30 +0200

    [ADD] I init the structure info in this file

---
+++ source/init_struct.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-AIA-200-PAR-2-1-n4s-maxime.carabina
+** File description:
+** init_struct
+*/
+
+#include <stdlib.h>
+#include "needforstek.h"
+
+info_t *init_struct(void)
+{
+    info_t *info = malloc(sizeof(info_t));
+
+    if (info == NULL)
+        return (NULL);
+    info->buff = NULL;
+    info->lidar = NULL;
+    info->size = 0;
+    info->finish = false;
+    return (info);
+}
