ab1f9942dd4023a5237b518bc14b01b1823b07ca
Maxime CARABINA <maxime.carabina@epitech.eu>
2021-05-10 16:23:31 +0200

    [UP] added new function

--- include/needforstek.h
+++ include/needforstek.h
@@ -8,5 +8,18 @@
 #ifndef MY_PROG_H_
 #define MY_PROG_H_
 
+#include <stdbool.h>
+#include <stddef.h>
+typedef struct info_s
+{
+    char *buff;
+    char **lidar;
+    size_t size;
+    bool finish;
+} info_t;
+
+info_t *init_struct(void);
+int print_cmd(info_t *info, char *str);
+int ai(info_t *info);
 
 #endif /* !MY_PROG_H_ */
