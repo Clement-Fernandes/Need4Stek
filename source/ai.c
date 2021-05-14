880ac5c37de3e8b6e8753a8b15ee3a7e3544aa3a
Maxime CARABINA <maxime.carabina@epitech.eu>
2021-05-14 16:53:25 +0200

    [UP] ai file

--- source/ai.c
+++ source/ai.c
@@ -6,23 +6,60 @@
 */
 
 #include <stdlib.h>
-#include "needforstek.h"
+#include <stdio.h>
 #include "my.h"
+#include "needforstek.h"
 
 static const int TAG = 35;
+static const int MID = 19;
+static const int LEFT = 3;
+static const int RIGHT = 34;
+
+static void end_track(info_t *info)
+{
+    if (my_strcmp(info->lidar[TAG], "Track Cleared") == true)
+        info->finish = true;
+}
+
+static void move_wheels(info_t *info)
+{
+    float i = 0;
+
+    if (atof(info->lidar[LEFT]) < atof(info->lidar[RIGHT])) {
+        i = (atof(info->lidar[LEFT]) - atof(info->lidar[RIGHT]) * 0.1);
+        print_cmd(info, "CAR_FORWARD:0.3\n");
+    }
+    if (atof(info->lidar[LEFT]) > atof(info->lidar[RIGHT])) {
+        i = (atof(info->lidar[LEFT]) - atof(info->lidar[RIGHT])) * 0.0;
+        print_cmd(info, "CAR_FORWARD:0.3\n");
+    }
+}
+
+// static void wheels_dir(info_t *info)
+// {
+//     if (atof(info->lidar[LEFT]) < atof(info->lidar[RIGHT])) {
+//         print_cmd(info, "WHEELS_DIR:-0.2\n");
+//         print_cmd(info, "CAR_FORWARD:0.4\n");
+//     }
+//     if (atof(info->lidar[LEFT]) > atof(info->lidar[RIGHT])) {
+//         print_cmd(info, "WHEELS_DIR:0.2\n");
+//         print_cmd(info, "CAR_FORWARD:0.4\n");
+//     }
+// }
 
 int ai(info_t *info)
 {
     while (info->finish == false) {
         print_cmd(info, "GET_INFO_LIDAR\n");
         info->lidar = my_str_to_word_arr(info->buff, ':');
-        if (atof(info->lidar[19]) > 500.0)
-            print_cmd(info, "CAR_FORWARD:0.5\n");
+        if (atof(info->lidar[MID]) > 2000.0)
+            print_cmd(info, "CAR_FORWARD:0.7\n");
         else
-            print_cmd(info, "CAR_FORWARD:0.0\n");
-        if (my_strcmp(info->lidar[TAG], "Track Cleared") == true)
-            info->finish = true;
+            move_wheels(info);
+        end_track(info);
         free_arr(info->lidar);
     }
+    print_cmd(info, "CAR_FORWARD:0.0\n");
+    print_cmd(info, "CYCLE_WAIT:5\n");
     return (0);
 }
